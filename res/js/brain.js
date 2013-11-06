57be3d78c5387f149b219a3bdee834efe64cfc19
Prateek Saxena <prtksxna@gmail.com>
2013-11-06 12:05:48 +0530

    Adding basic presets

---
+++ res/js/brain.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".filter-icon").tooltip();
+    $("table.filter-icon-set td").click(function(){
+	$(this).toggleClass("selected");
+    });
+
+    $("#presets li").click(function(){
+	$("#presets li").removeClass("active");
+	$(this).addClass("active");
+    });
+});
