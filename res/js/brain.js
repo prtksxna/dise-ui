dae9430005b4a150d54725b148fd2c732625c4ae
Prateek Saxena <prtksxna@gmail.com>
2013-11-26 18:04:33 +0530

    Adding an overlay for the preset editor #1

--- res/js/brain.js
+++ res/js/brain.js
@@ -15,6 +15,9 @@ $(function(){
         if(!confirm("Are you sure? This action cannot be undone!")) return false;
     });
 
+    $("#presets li .glyphicon-cog").click(function(){
+        $("#preset-editor").toggleClass("activate");
+    });
 
     $("#control_toggle").click(function(e){
         $("#controls").toggleClass("hidden_controls");
@@ -26,5 +29,13 @@ $(function(){
     var class_array = ["a", "b", "c", "d", "e"]
     $("ol.top-breadcrumbs li").each(function(i, e){
         $(e).addClass(class_array[i]);
+    });
+
+    // Sliders
+    $(".range-slider").slider({
+        range: true,
+        min: 0,
+        max: 30,
+        values: [10,20]
     });
 });
